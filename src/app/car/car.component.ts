8ded4d27a301fcbb87ce5bf69040f86e88e55b6e
a.bubelev <bubeleu@gmail.com>
2022-06-08 20:38:25 +0300

    name thing

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -2,23 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-car',
-  // templateUrl: './car.component.html',
-  template: `
-  <div>My favorite car</div>
-  <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque distinctio officia corporis, deleniti modi aliquam cumque odit magni tempora nemo mollitia iusto facere reprehenderit saepe exercitationem esse minus quod iste.</h3>
-  `,
-  // styleUrls: ['./car.component.css']
-  styles: [`
-  h3{
-    color: coral
-  }
-  `]
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+
 })
-export class CarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class CarComponent {
+  carName: string = 'Ford';
+  carYear: number = 2021;
 
 }
