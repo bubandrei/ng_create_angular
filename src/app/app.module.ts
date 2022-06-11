53090a051c1348b2ecdbb0658ac12ed77a6f2f0f
a.bubelev <bubeleu@gmail.com>
2022-06-11 23:04:35 +0300

    create data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CarsComponent } from './cars/cars.component';
 import { CarComponent } from './car/car.component';
 import { FormsModule } from '@angular/forms';
+import { PhoneComponent } from './phones/phones.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CarsComponent,
-    CarComponent
+    CarComponent,
+    PhoneComponent
   ],
   imports: [
     BrowserModule,
