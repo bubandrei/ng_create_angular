8f187c84391b09bc091b90735408e694879d13ca
a.bubelev <bubeleu@gmail.com>
2022-06-11 22:21:51 +0300

    create nwe files for cars

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,46 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+// import { style } from "@angular/animations";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-cars',
-  templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.css']
+    selector: 'app-cars',
+    // templateUrl: './cars.component.html',
+    template: "<h2>Hello</h2>",
+    styleUrls: ['./cars.component.css']
+
 })
+
 export class CarsComponent {
-
-  // canAddCar: boolean = false;
-  // addCarStatus = '';
-  // inputText = 'Default text';
-  carName = '';
-  addCarStatus = false;
-  cars: [string, string, string] = ['Ford', 'Audi', 'BMW'];
-  // items = [{ id: 0, name: 111 }, { id: 2, name: 222 }, { id: 33, name: 333 }]
-  items = [
-    new Date(2022, 5, 4),
-    new Date(2023, 6, 5),
-    new Date(2024, 7, 6),
-  ]
-
-  constructor() {
-    // setTimeout(() => {
-    //   this.canAddCar = !this.canAddCar;
-    // },4000)
-  }
-  // addCar() {
-  //   this.addCarStatus = "Car added";
-  // };
-  // onKeyUp(value: string) {
-  //   this.inputText = value;
-  // };
-  //   onKeyUp(event: Event) {
-  //   this.inputText = (<HTMLInputElement>event.target).value;
-  // }
-  addCar() {
-    this.addCarStatus = true;
-    this.cars.push(this.carName);
-    this.carName = '';
-  };
-  setBigText(car:string) {
-    return car.length > 2 ? true : false;
-  }
-
+    constructor(){
+        
+    }
 }
